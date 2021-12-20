34d9332a0fe5b23414eeac0c2193e92cea3b24e3
askkostya <askkostya@gmail.com>
2021-12-20 15:02:22 +1200

    Добавлен новый функционал 6 - Модель ККТ 7 - Версия прошивки

--- kktatol.py
+++ kktatol.py
@@ -5,13 +5,21 @@ import time
 
 fptr = IFptr("")
 
-i = 1
-while i < 10:
-	fptr.open()
-	if fptr.isOpened():
-		break
-	time.sleep(5)
-	i = i+i
+
+def connectToKKT():
+	if len(sys.argv) == 4:  # Передан IP адрес и порт
+		fptr.setSingleSetting(IFptr.LIBFPTR_SETTING_PORT, str(IFptr.LIBFPTR_PORT_TCPIP))
+		fptr.setSingleSetting(IFptr.LIBFPTR_SETTING_IPADDRESS, sys.argv[2])
+		fptr.setSingleSetting(IFptr.LIBFPTR_SETTING_IPPORT, sys.argv[3])
+		fptr.applySingleSettings()
+	i = 1
+	while i < 10:
+		fptr.open()
+		if fptr.isOpened():
+			return 1
+		else:
+			time.sleep(5)
+			i = i + i
 
 
 def deltadate(paramAsDateTime):
@@ -49,11 +57,29 @@ def getDataFromKKT(param):
 		fptr.setParam(IFptr.LIBFPTR_PARAM_FN_DATA_TYPE, IFptr.LIBFPTR_FNDT_OFD_EXCHANGE_STATUS)
 		fptr.fnQueryData()
 		print(deltadate(fptr.getParamDateTime(IFptr.LIBFPTR_PARAM_DATE_TIME))*(-1))
+	# Модель ККТ
+	if param == "6":
+		fptr.setParam(IFptr.LIBFPTR_PARAM_DATA_TYPE, IFptr.LIBFPTR_DT_STATUS)
+		fptr.queryData()
+		print(fptr.getParamString(IFptr.LIBFPTR_PARAM_MODEL_NAME))
+	# Версия прошивки
+	if param == "7":
+		fptr.setParam(IFptr.LIBFPTR_PARAM_DATA_TYPE, IFptr.LIBFPTR_DT_STATUS)
+		fptr.queryData()
+		print(fptr.getParamString(IFptr.LIBFPTR_PARAM_UNIT_VERSION))
 
 
-try:
-	getDataFromKKT(sys.argv[1])
-except IndexError:
-	print("No parameter")
+def main():
+	if len(sys.argv) == 1:
+		print("No parameter")
+		sys.exit()
+	connectResult = connectToKKT()
+	if connectResult == 1:
+		getDataFromKKT(sys.argv[1])
+	else:
+		print("No connection")
+	fptr.close()
 
-fptr.close()
+
+if __name__ == '__main__':
+	main()
